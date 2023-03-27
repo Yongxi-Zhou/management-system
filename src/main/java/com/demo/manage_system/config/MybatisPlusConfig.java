6f26b3225d2fff277a2452abb18d1a7b1f3a01f1
wryan <chow971@outlook.com>
2023-03-27 18:23:38 -0400

    add config

---
+++ src/main/java/com/demo/manage_system/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.demo.manage_system.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.demo.manage_system.mapper")
+public class MybatisPlusConfig {
+  public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+    interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+    return interceptor;
+  }
+
+  @Bean
+  public ConfigurationCustomizer configurationCustomizer() {
+    return configuration -> configuration.setUseDeprecatedExecutor(false);
+  }
+
+}
---
+++ src/test/java/com/demo/manage_system/ManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.manage_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
