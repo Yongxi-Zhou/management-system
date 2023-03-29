655d8877791b490d3eb6b8ad75f6b74fc8c7fb42
wryan <chow971@outlook.com>
2023-03-29 16:38:21 -0400

    add logic for sending response with picture and key

---
+++ src/main/java/com/demo/manage_system/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.demo.manage_system.controller;
+import cn.hutool.core.map.MapUtil;
+import com.demo.manage_system.common.lang.Const;
+import com.demo.manage_system.common.lang.Result;
+import com.google.code.kaptcha.Producer;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.imageio.ImageIO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sun.misc.BASE64Encoder;
+
+@RestController
+public class AuthController extends BaseController {
+
+  @Autowired
+  Producer producer;
+
+  @GetMapping("/captcha")
+  public Result captcha() throws IOException {
+    String key = UUID.randomUUID().toString();
+//    生成验证码
+    String code = producer.createText();
+
+    BufferedImage image = producer.createImage(code);
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    ImageIO.write(image, "jpg", outputStream);
+
+    BASE64Encoder encoder = new BASE64Encoder();
+    String str = "data:image/jpeg;base64," + encoder.encode(outputStream.toByteArray());
+
+    redisUtil.hset(Const.CAPTCHA_KEY, key, code, 120);
+
+//    返回key和验证码图片
+    return Result.succ(
+        MapUtil.builder().put("token", key).put("captchaImg", str).build()
+    );
+  }
+
+}
--- src/main/java/com/demo/manage_system/controller/BaseController.java
+++ src/main/java/com/demo/manage_system/controller/BaseController.java
@@ -1,5 +1,14 @@
 package com.demo.manage_system.controller;
 
+import com.demo.manage_system.util.RedisUtil;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class BaseController {
+  @Autowired
+  HttpServletRequest req;
+
+  @Autowired
+  RedisUtil redisUtil;
 
 }
